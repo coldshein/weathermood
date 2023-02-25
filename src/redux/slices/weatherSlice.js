14edf7513e92df4d1ac232e5240af1b6c7837390
Roman <coldshein@gmail.com>
2023-02-26 00:48:36 +0200

    redux

---
+++ src/redux/slices/weatherSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    weather: [],
+}
+
+export const weatherSlice = createSlice({
+    name: weather,
+    initialState,
+    reducers:{
+
+    }
+})
+
+export const {} = weatherSlice.actions
+
+export default weatherSlice.reducer
